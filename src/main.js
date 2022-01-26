4d3d6484eaf56c6dc74cce7dea8b5d26531149bc
AlanWork <528148291@qq.com>
2022-01-26 20:22:59 +0800

    i18n config

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import i18n from './i18n'
 
+Vue.use(VueI18n)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
+  VueI18n,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
