f1a1430cbb0f8253a2b1e5837e836a4d3d8bdab5
AlanWork <528148291@qq.com>
2022-01-26 21:43:26 +0800

    i18n config --- cn

--- src/main.js
+++ src/main.js
@@ -5,12 +5,11 @@ import router from './router'
 import store from './store'
 import i18n from './i18n'
 
-Vue.use(VueI18n)
+Vue.use(i18n)
 Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  VueI18n,
   i18n,
   render: h => h(App)
 }).$mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
+    $t('message')
     <h1>This is an about page</h1>
   </div>
 </template>
